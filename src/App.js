536d99cd0ee2242ccccbcfc537193e13d403878e
r0mansCode <kovaljonokroman@gmail.com>
2021-10-24 18:49:19 +0300

    splitting the page into components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './App.scss';
+import './Components/Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
+import Contents from './Components/Contents/Contents';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      Category name
+      <Navbar />
+      <Contents />
     </div>
   )}
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <h1>Navigation bar</h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
