63ee541ee2bdab5d579719e48ae4f08a9a4dd27a
r0mansCode <kovaljonokroman@gmail.com>
2021-11-25 12:48:18 +0200

    Adding a comment in App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       this.setState({index: this.state.productCart.push({id, amount})})
   }
 
+  //handleClick not working correct, will fix it in the future
    handleClickRemove (index){
     const newTotalAmount = this.state.productCart.filter((_, i) =>  i !== index);
     this.setState({ productCart: newTotalAmount});
